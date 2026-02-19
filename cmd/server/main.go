b1c30b953afd7c0c950eb2d02184bb6cd0e77ee4
AfshinNajafi74 <afshinnajafi74@gmail.com>
2026-02-19 12:38:40 +0330

    feat(http): add user register & login handlers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 
 	"github.com/AfshinNajafi74/go-gymApp/internal/config"
+	"github.com/AfshinNajafi74/go-gymApp/internal/domain/user"
+	userHttp "github.com/AfshinNajafi74/go-gymApp/internal/handler/http"
 	"github.com/AfshinNajafi74/go-gymApp/internal/repository/postgres"
 	"github.com/AfshinNajafi74/go-gymApp/pkg/database"
 	"github.com/gorilla/mux"
@@ -24,13 +26,13 @@ func main() {
 	userRepo := postgres.NewUserRepository(db)
 
 	userService := user.NewService(userRepo)
+	userHandler := userHttp.NewUserHandler(userService)
 
 	// Router
 	r := mux.NewRouter()
 
-	r.HandleFunc("/health", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("gym app is running"))
-	})
+	r.HandleFunc("/register", userHandler.Register).Methods("POST")
+	r.HandleFunc("/login", userHandler.Login).Methods("POST")
 
 	log.Println("Server running on : " + cfg.Port)
 	log.Fatal(http.ListenAndServe(":"+cfg.Port, r))
---
+++ internal/handler/http/user_handler.go
@@ -0,0 +1,58 @@
+package http
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/AfshinNajafi74/go-gymApp/internal/domain/user"
+)
+
+type UserHandler struct {
+	service user.Service
+}
+
+func NewUserHandler(s user.Service) *UserHandler {
+	return &UserHandler{service: s}
+}
+
+func (h *UserHandler) Register(w http.ResponseWriter, r *http.Request) {
+	var req struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err := h.service.Register(req.Name, req.Email, req.Password)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (h *UserHandler) Login(w http.ResponseWriter, r *http.Request) {
+	var req struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	user, err := h.service.Login(req.Email, req.Password)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	json.NewEncoder(w).Encode(user)
+}
